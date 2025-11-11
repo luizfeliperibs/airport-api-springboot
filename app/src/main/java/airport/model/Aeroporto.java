2900518b37e65fa518dafdaffb059ce2a2730020
Luiz Felipe Ribeiro Souza <lipezerafar@gmail.com>
2025-11-11 12:11:52 -0300

    feat: termina estado inicial da classe Aeroporto

--- app/src/main/java/airport/model/Aeroporto.java
+++ app/src/main/java/airport/model/Aeroporto.java
@@ -1,11 +1,12 @@
 package airport.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -14,4 +15,30 @@ import lombok.Setter;
 @Entity(name="aeroporto")
 @Table(name="aeroportos")
 public class Aeroporto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome_aeroporto")
+    private String nomeAeroporto;
+
+    @Column(name = "codigo_iata")
+    private String codigoIata;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "codigo_pais_iso")
+    private String codigoPaisIso;
+
+    @Column(name = "latitude")
+    private BigDecimal latitude;
+
+    @Column(name = "longitude")
+    private BigDecimal longitude;
+
+    @Column(name = "altitude")
+    private BigDecimal altitude;
+
 }
