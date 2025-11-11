0f34af167e597d7af038fff18087b2be2fd16c67
luizfeliperibs <lipezerafar@gmail.com>
2025-11-11 05:40:23 -0300

    feat: implementa classe buscarPorId

--- app/src/main/java/airport/controller/AeroportoController.java
+++ app/src/main/java/airport/controller/AeroportoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aeroportos")
@@ -25,6 +26,20 @@ public class AeroportoController {
     @GetMapping
     public List<Aeroporto> listarAeroportos() {
         return service.listarAeroportos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aeroporto> buscarPorId(@PathVariable Long id) {
+
+        Optional<Aeroporto> aeroportoOptional = service.buscarAeroportoById(id);
+
+        if (aeroportoOptional.isPresent()) {
+            Aeroporto aeroportoEncontrado = aeroportoOptional.get();
+            return ResponseEntity.ok(aeroportoEncontrado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
     @PostMapping
