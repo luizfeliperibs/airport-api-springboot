45e233c17e9accf2baed281f830557f47ba6cfef
luizfeliperibs <lipezerafar@gmail.com>
2025-11-12 21:27:52 -0300

    feat: implementa tratamento de erros (404) e validação de DTOs (400)

--- app/src/main/java/airport/infra/TratadorDeErros.java
+++ app/src/main/java/airport/infra/TratadorDeErros.java
@@ -1,12 +1,17 @@
 package airport.infra;
 
 import airport.exceptions.AeroportoNaoEncontradoException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -24,6 +29,29 @@ public class TratadorDeErros extends ResponseEntityExceptionHandler {
         );
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> body = Map.of(
+                "status", status.value(), // 400
+                "error", "Bad Request",
+                "message", "Dados de entrada inválidos.",
+                "validation_errors", errors // Mapa com os erros detalhados
+        );
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
 }
